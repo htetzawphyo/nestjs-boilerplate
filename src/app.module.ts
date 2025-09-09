2cb280acef490b8f9407ed5d7cb48cf677d85f60
nyein phyo aung <nyeinphyoaung.edu@gmail.com>
2025-09-09 23:32:40 +0630

    feat: add create, update and get by id endpoints for roles

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+import { RoleModule } from './role/role.module';
 
 @Module({
   imports: [
@@ -15,12 +16,13 @@ import { LoggerMiddleware } from './common/middlewares/logger.middleware';
     PrismaModule,
     AuthModule,
     UserModule,
+    RoleModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('*');
+    consumer.apply(LoggerMiddleware).forRoutes('*path');
   }
 }
